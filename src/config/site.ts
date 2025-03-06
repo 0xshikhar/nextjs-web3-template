c275f1546a78c5985e56fd7ef00ce95414377841
Shikhar Singh <0xshikhar@gmail.com>
2025-03-07 01:49:40 +0530

    feat: site config

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,17 +3,17 @@ import { SiteConfig } from "@/types"
 import { env } from "@/env.mjs"
 
 export const siteConfig: SiteConfig = {
-  name: "Next Entree",
-  author: "redpangilinan",
+  name: "Next Web3 Template",
+  author: "0xShikhar",
   description:
     "Next.js 14+ starter template with app router, shadcn/ui, typesafe env, icons and configs setup.",
-  keywords: ["Next.js", "React", "Tailwind CSS", "Radix UI", "shadcn/ui"],
+  keywords: ["Next.js", "React", "Tailwind CSS", "RainbowKit", "wagmi", "Radix UI", "shadcn/ui"],
   url: {
     base: env.NEXT_PUBLIC_APP_URL,
-    author: "https://rdev.pro",
+    author: "https://shikhar.xyz",
   },
   links: {
-    github: "https://github.com/redpangilinan/next-entree",
+    github: "https://github.com/0xshikhar/next-web3-template",
   },
   ogImage: `${env.NEXT_PUBLIC_APP_URL}/og.jpg`,
 }
