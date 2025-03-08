66b38955b75f51a4033d379dd0721e7af19794a8
Shikhar Singh <0xshikhar@gmail.com>
2025-03-08 17:21:50 +0530

    feat: contract verified

--- src/lib/contracts.ts
+++ src/lib/contracts.ts
@@ -7,6 +7,12 @@ export const contractAddresses = {
     tokenMint: "0x3BfaD8D88E5F8f86b9cA1A0A29bcb4f083D1E60A",
 };
 
+// block explorer - blockscout
+export const blockExplorer = {
+    nft: "https://eth-sepolia.blockscout.com/address/0x73E2dCc244FA575C143F886b9804321b4C0061b4?tab=contract",
+    tokenMint: "https://eth-sepolia.blockscout.com/address/0x3BfaD8D88E5F8f86b9cA1A0A29bcb4f083D1E60A?tab=contract",
+};
+
 
 export const contractABIs = {
     nft: nftABI,
